eff3af9e528dbe3f70034fb032f7e5f1e1ff3cdd
Yonchi03 <JoanDenchev@gmail.com>
2021-03-20 20:16:41 +0200

    LinkedList/ArrayList Calculate Time

--- Project/Run.java
+++ Project/Run.java
@@ -1,19 +1,57 @@
 package Project;
 
+import java.text.*;
+
 public class Run {
 
     public static void main(String[] args) {
 
+        ///////////////////////////////////////////////////////////////////////
+        long startTime1 = System.nanoTime();
+
         //Creating house objects
-        House House1 = new House(250.6f, "Location 1", 3, "\"The owner's name\"");
-        House House2 = new House(410.2f, "Location 2", 5, "\"The owner's name\"");
-        House House3 = new House(350.5f, "Location 3", 3, "\"The owner's name\"");
-        House House4 = new House(380.0f, "Location 4", 4, "\"The owner's name\"");
+        for (int i = 0; i < 300000; i++){
+            House House1 = new House(250.6f, "Location 1", 3, "\"The owner's name\"", false);
+            House House2 = new House(410.2f, "Location 2", 5, "\"The owner's name\"", false);
+            House House3 = new House(350.5f, "Location 3", 3, "\"The owner's name\"", false);
+            House House4 = new House(380.0f, "Location 4", 4, "\"The owner's name\"", false);
+        }
 
-        //Printing all houses
-        for (House i : House.getHouseList()) i.fancyPrint();
+        for (House i : House.getHouseArrayList()) i.fancyPrint();
+        House.printMostSpaciousAL();
 
-        //Printing the most spacious house
-        House.printMostSpacious();
+        long endTime1 = System.nanoTime();
+
+
+        ///////////////////////////////////////////////////////////////////////
+        long startTime2 = System.nanoTime();
+
+        for (int i = 0; i < 300000; i++){
+            House House1 = new House(250.6f, "Location 1", 3, "\"The owner's name\"", true);
+            House House2 = new House(410.2f, "Location 2", 5, "\"The owner's name\"", true);
+            House House3 = new House(350.5f, "Location 3", 3, "\"The owner's name\"", true);
+            House House4 = new House(380.0f, "Location 4", 4, "\"The owner's name\"", true);
+        }
+
+        for (House i : House.getHouseLinkedList()) i.fancyPrint();
+        House.printMostSpaciousLL();
+
+        long endTime2 = System.nanoTime();
+
+
+        ///////////////////////////////////////////////////////////////////////
+        long duration1 = (endTime1 - startTime1);
+        long duration2 = (endTime2 - startTime2);
+
+        System.out.printf("""
+                ArrayList  Duration: %s
+                LinkedList Duration: %s
+                """, customFormat(duration1).replace(',', ' '),
+                     customFormat(duration2).replace(',', ' '));
+    }
+
+    static public String customFormat(long value) {
+        DecimalFormat myFormatter = new DecimalFormat("###,###");
+        return myFormatter.format(value);
     }
 }
