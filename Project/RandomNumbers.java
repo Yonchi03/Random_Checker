ff7dbef09f89bd4016474f641f00300019f35247
Yonchi03 <JoanDenchev@gmail.com>
2021-03-22 14:42:03 +0200

    Version 1.1

--- Project/RandomNumbers.java
+++ Project/RandomNumbers.java
@@ -10,18 +10,19 @@ public class RandomNumbers {
     static ArrayList<Byte> list = new ArrayList<>();
     static byte elementCounter = 20;
 
-    public static int getRandomInteger(int min, int max) {
-        return (int) ((Math.random() * ((max - min) + 1)) + min);
+    public static byte getRandom(byte min, byte max) {
+        return (byte) ((Math.random() * ((max - min) + 1)) + min);
     }
 
     public static void main(String[] args) {
-        for (int i = 1; i < 11; i++) {
-            list.add((byte) i);
+        for (byte i = 1; i < 11; i++) {
+            list.add(i);
         }
         for (Byte l : list) {
             System.out.print(l + " ");
         }
         System.out.println();
+
         remover();
     }
 
@@ -38,7 +39,7 @@ public class RandomNumbers {
     }
 
     private static void remover(){
-        byte b = (byte) getRandomInteger(1, 10);
+        byte b = getRandom((byte) 1, (byte) 10);
         if (isItInArrayList(b)){
             list.remove(elementCounter);
             System.out.println("Got: " + b);
