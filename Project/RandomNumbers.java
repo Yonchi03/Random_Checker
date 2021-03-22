63a1d8d52ac7af5734bd9c4d0164c7d00d4b9867
Yonchi03 <JoanDenchev@gmail.com>
2021-03-22 15:56:37 +0200

    Version 2.0

--- Project/RandomNumbers.java
+++ Project/RandomNumbers.java
@@ -1,14 +1,11 @@
 package Project;
 
-import org.jetbrains.annotations.Contract;
-
 import java.util.ArrayList;
 
 
 public class RandomNumbers {
 
     static ArrayList<Byte> list = new ArrayList<>();
-    static byte elementCounter;
 
     public static byte getRandom(byte min, byte max) {
         return (byte) ((Math.random() * ((max - min) + 1)) + min);
@@ -26,22 +23,10 @@ public class RandomNumbers {
         remover();
     }
 
-    @Contract(pure = true)
-    private static Boolean isItInArrayList(byte b){
-        boolean k = false;
-        for (Byte i : list) {
-            if (i == b) {
-                k = true;
-                elementCounter = (byte) (list.indexOf(i));
-            }
-        }
-        return k;
-    }
-
     private static void remover(){
-        byte b = getRandom((byte) 1, (byte) 10);
-        if (isItInArrayList(b)){
-            list.remove(elementCounter);
+        Byte b = getRandom((byte) 1, (byte) 10);
+        if (list.contains(b)){
+            list.remove(b);
             System.out.println("Got: " + b);
         }
         if (list.isEmpty()) {
